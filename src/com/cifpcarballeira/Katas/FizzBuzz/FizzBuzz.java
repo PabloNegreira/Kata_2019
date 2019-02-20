b7c8e686132d99d4b1e1b0eac03187e2710bbf1e
Pablo Negreira <ronepablo10@gmail.com>
2019-02-20 10:42:15 +0100

    2 devuelve 2

--- src/com/cifpcarballeira/Katas/FizzBuzz/FizzBuzz.java
+++ src/com/cifpcarballeira/Katas/FizzBuzz/FizzBuzz.java
@@ -2,7 +2,12 @@ package com.cifpcarballeira.Katas.FizzBuzz;
 public class FizzBuzz {
 
     public String getFizzBuzz(int num) {
-        return "1";
+        if(num==1){
+            return "1";
+        }else {
+            return "2";
+        }
+        
     }
     
 }
--- test/com/cifpcarballeira/Katas/FizzBuzz/Test/Test_FizzBuzz.java
+++ test/com/cifpcarballeira/Katas/FizzBuzz/Test/Test_FizzBuzz.java
@@ -10,11 +10,16 @@ public class Test_FizzBuzz {
     public Test_FizzBuzz() {
     }
     
-    
     @Test
     public void test1devuelve1() {
         FizzBuzz fb = new FizzBuzz();
         String resultado= fb.getFizzBuzz(1);
         assertEquals("1",resultado);
+    }
+    @Test
+    public void test2devuelve2() {
+        FizzBuzz fb = new FizzBuzz();
+        String resultado= fb.getFizzBuzz(2);
+        assertEquals("2",resultado);
     }
 }
