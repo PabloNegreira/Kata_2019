ec8d10049850f1b8e45d814e7e3ff4f750a8867f
Pablo Negreira <ronepablo10@gmail.com>
2019-02-20 12:21:51 +0100

    4 devuelve Buzz

--- src/com/cifpcarballeira/Katas/FizzBuzz/FizzBuzz.java
+++ src/com/cifpcarballeira/Katas/FizzBuzz/FizzBuzz.java
@@ -3,13 +3,18 @@ package com.cifpcarballeira.Katas.FizzBuzz;
 public class FizzBuzz {
 
     public String getFizzBuzz(int num) {
-        if (num % 3 == 0) {
-            return "Fizz";
-        }
-        if (num == 1) {
-            return "1";
+        if (num % 3 == 0 && num % 5 == 0) {
+            return "Fizz Buzz";
         } else {
-            return "2";
+            if (num % 5 == 0) {
+                return "Buzz";
+            } else {
+                if (num % 3 == 0) {
+                    return "Fizz";
+                } else {
+                    return Integer.toString(num);
+                }
+            }
         }
     }
 }
--- test/com/cifpcarballeira/Katas/FizzBuzz/Test/Test_FizzBuzz.java
+++ test/com/cifpcarballeira/Katas/FizzBuzz/Test/Test_FizzBuzz.java
@@ -4,29 +4,36 @@ import com.cifpcarballeira.Katas.FizzBuzz.FizzBuzz;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-
 public class Test_FizzBuzz {
-    
+
     public Test_FizzBuzz() {
     }
-    
+
     @Test
     public void test1devuelve1() {
         FizzBuzz fb = new FizzBuzz();
-        String resultado= fb.getFizzBuzz(1);
-        assertEquals("1",resultado);
+        String resultado = fb.getFizzBuzz(1);
+        assertEquals("1", resultado);
     }
+
     @Test
     public void test2devuelve2() {
         FizzBuzz fb = new FizzBuzz();
-        String resultado= fb.getFizzBuzz(2);
-        assertEquals("2",resultado);
+        String resultado = fb.getFizzBuzz(2);
+        assertEquals("2", resultado);
     }
+
     @Test
     public void test3devuelveFizz() {
         FizzBuzz fb = new FizzBuzz();
-        String resultado= fb.getFizzBuzz(3);
-        assertEquals("Fizz",resultado);
+        String resultado = fb.getFizzBuzz(3);
+        assertEquals("Fizz", resultado);
     }
 
+    @Test
+    public void test4devuelveBuzz() {
+        FizzBuzz fb = new FizzBuzz();
+        String resultado = fb.getFizzBuzz(5);
+        assertEquals("Buzz", resultado);
+    }
 }
